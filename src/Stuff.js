0dce3331e966ec6296ae11e0f23818c4c72dd7bc
wdguevarag <wdguevarag@gmail.com>
2020-09-26 12:07:28 -0500

    Add files via upload

---
+++ src/Stuff.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Stuff extends Component {
+    render() {
+        return (
+            <div>
+                <h2>MENU 2</h2>
+            </div>
+        );
+    }
+}
+
+export default Stuff;
